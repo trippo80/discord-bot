5eea25cfb3163ef9b8a84ae7e1e22826e53e8744
magnus <magnus@paloo.se>
2017-12-19 10:01:25 +0100

    fika: minor modification to check if we got a minute argument or not

--- plugins/fika.js
+++ plugins/fika.js
@@ -1,5 +1,6 @@
 const moment = require('moment');
 const helpers = require('../lib/helpers');
+const _ = require('lodash');
 
 // setup data object
 let data = {
@@ -27,7 +28,7 @@ module.exports = {
         if (args[0]) {
             // Yes - convert to minutes.
             let minutes = parseInt(args[0]);
-            if (! minutes || minutes < 0) {
+            if (! _.isInteger(minutes) || minutes < 0) {
                 // Argument doesn't seem to be minutes.
                 message.channel.send('<@!' + message.author.id + '> Förstod inte det där..');
                 return;
