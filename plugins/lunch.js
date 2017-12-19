60b3d52f4e8a7d7906f0b560cf9f3457088f41dc
Hampus <nordin.hampus@gmail.com>
2017-12-19 18:51:40 +0100

    Updated lunch plugin to take hour and minute format instead of relative num minutes.

--- plugins/lunch.js
+++ plugins/lunch.js
@@ -8,6 +8,13 @@ const moment = require('moment');
 const helpers = require('../lib/helpers');
 const _ = require('lodash');
 
+const validTimeFormats = [
+  'HH.mm',
+  'HH:mm',
+  'HH.mm.ss',
+  'HH:mm:ss'
+];
+
 // setup data object
 let data = {
     lunch: {
@@ -31,52 +38,55 @@ module.exports = {
             return;
         }
 
+        let now = new Date();
+
         // Did we get an argument?
         if (args[0]) {
-            // Yes - convert to minutes.
-            let minutes = parseInt(args[0]);
-            if (! _.isInteger(minutes) || minutes < 0) {
-                // Argument doesn't seem to be minutes.
-                message.channel.send('<@!' + message.author.id + '> Förstod inte det där..');
+            let time = moment(args[0], validTimeFormats, true);
+            let now;
+
+            if (!time.isValid()) {
+                message.channel.send(`<@!${message.author.id}> Förstod inte det där..`);
                 return;
             }
 
-            data.lunch.place = (args[1] + '').trim() || null;
+            // Assume next day if time has already passed today.
+            if (time.isBefore(now)) {
+                time.add(1, 'd');
+            }
+
+            data.lunch.place = args.length > 1 ? args.slice(1).join(' ') : null;
 
             // Save minutes and return message.
-            data.lunch.next = moment().add(minutes, 'm');
-            message.channel.send('<@!' + message.author.id + '> Ok, avfärd från sajens om ' + minutes + ' minut(er)' + (data.lunch.place ? ' mot ' + data.lunch.place : '') + '!');
+            data.lunch.next = time;
+            message.channel.send(`<@!${message.author.id}> Ok, avfärd från sajens ${time.fromNow()}${(data.lunch.place ? ' mot ' + data.lunch.place : '')}!`);
 
             // Clear existing timer before creating a new one.
             if (data.lunch.timer !== null) {
-              clearInterval(data.lunch.timer);
+                clearTimeout(data.lunch.timer);
             }
 
             // Create timer
             data.lunch.timer = setTimeout(function () {
-                clearInterval(data.lunch.interval);
                 message.channel.send('@everyone Nu är det dags att gå' + (data.lunch.place ? ' till ' + data.lunch.place : '') + '!!');
                 data.lunch.next = null;
                 data.lunch.timer = null;
-            }, moment(data.lunch.next).diff(moment(), 'ms'));
+            }, time.diff(now, 'ms'));
 
         } else {
             // No argument, do we have a next lunch set?
             if (data.lunch.next) {
-                // Yes - calculate number of minutes until lunch.
-                let minutes = moment(data.lunch.next).diff(moment(), 'm');
-
                 // Make sure we haven't missed lunch.
-                if (minutes > 0) {
-                    message.channel.send('<@!' + message.author.id + '> avfärd för lunch om ' + minutes + ' minut(er)' + (data.lunch.place ? ' mot ' + data.lunch.place : '') + '!');
+                if (data.lunch.next.isAfter(now)) {
+                    message.channel.send(`<@!${message.author.id}> avfärd för lunch från sajens ${data.lunch.next.fromNow()}${data.lunch.place ? ' mot ' + data.lunch.place : ''}!`);
                 } else {
                     // We missed lunch - reset and return a message.
                     data.lunch.next = null;
-                    message.channel.send('<@!' + message.author.id + '> Vet inte när nästa lunch är :(');
+                    message.channel.send(`<@!${message.author.id}> Vet inte när nästa lunch är :(`);
                 }
             } else {
                 // No lunch set - return message.
-                message.channel.send('<@!' + message.author.id + '> Vet inte när nästa lunch är :(');
+                message.channel.send(`<@!${message.author.id}> Vet inte när nästa lunch är :(`);
             }
         }
     }
